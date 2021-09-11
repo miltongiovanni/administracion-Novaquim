5860803dd9cdb40bfb57f88c3987496a5b466dfc
miltongiovanni <miltongiovanni@gmail.com>
2021-09-11 06:31:34 -0400

    Configuracion inicial plantilla
    Creation formulario login

--- assets/app.js
+++ assets/app.js
@@ -18,8 +18,10 @@ require('bootstrap');
 
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
-// require('bootstrap/js/dist/popover');
+//require('bootstrap/js/dist/popover');
 
+/*
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
 });
+*/
