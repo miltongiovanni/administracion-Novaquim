5e78815978d8873abff5b5eba97a2f873dbac757
miltongiovanni <miltongiovanni@gmail.com>
2022-06-18 07:53:01 -0400

    envio de correo

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -39,10 +43,27 @@ class SecurityController extends AbstractController
     /**
      * @Route("/send-passcode", name="app_send_passcode")
      */
-    public function sendPasscode(UserRepository $userRepository): Response
+    public function sendPasscode(Request $request, UserRepository $userRepository, EntityManagerInterface $entityManager, MailerInterface $mailer): Response
     {
+        $email = $request->request->get('email');
         $passcode = rand(10000, 49999) + rand(1, 50000);
-        dd($passcode);
+        $user = $userRepository->findOneBy(['email' => $email]);
+        $user->setPasscode($passcode);
+        $entityManager->persist($user);
+        $entityManager->flush();
+        $email = (new Email())
+            ->from('contacto@novaquim.com')
+            ->to($email)
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+        dd($email,$passcode, $user);
         return $this->render('security/forgot-password.html.twig', [ 'title' => 'Contraseña olvidada', 'web_title' => 'Industrias Novaquim S.A.S.']);
     }
 
